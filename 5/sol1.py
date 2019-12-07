0d62d3f314b7853bdc37b45bad73e9f23c9c4bcb
RustingSword <i@toonaive.me>
2019-12-07 17:20:05 +0800

    fix error in day 6

--- 5/sol1.py
+++ 5/sol1.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-data = list(map(int, open('input').read().strip('\n').split(',')))
+data = list(map(int, open("input").read().strip("\n").split(",")))
 assert data[0] == 3
 data[data[1]] = 1  # input ID 1
 
 
 def _parse_code(code):
     code = str(code)
-    code = '0' * (5 - len(code)) + code
+    code = "0" * (5 - len(code)) + code
     op = int(code[-2:])
     modes = list(reversed(code[:-2]))
     return op, modes
 
 
 def _get_param(mode, index):
-    assert mode in '01'
-    return data[index] if mode == '1' else data[data[index]]
+    assert mode in "01"
+    return data[index] if mode == "1" else data[data[index]]
 
 
 i = 2
@@ -25,11 +25,11 @@ while i < len(data):
     if op == 1 or op == 2:
         first = _get_param(modes[0], i + 1)
         second = _get_param(modes[1], i + 2)
-        outpos = data[i+3]
+        outpos = data[i + 3]
         data[outpos] = first + second if op == 1 else first * second
         i += 4
     elif op == 4:
-        print(data[data[i+1]])
+        print(data[data[i + 1]])
         i += 2
     else:
         assert op == 99
--- 5/sol2.py
+++ 5/sol2.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 
-data = list(map(int, open('input').read().strip('\n').split(',')))
+data = list(map(int, open("input").read().strip("\n").split(",")))
 assert data[0] == 3
 data[data[1]] = 5  # input ID 5
 
 
 def _parse_code(code):
     code = str(code)
-    code = '0' * (5 - len(code)) + code
+    code = "0" * (5 - len(code)) + code
     op = int(code[-2:])
     modes = list(reversed(code[:-2]))
-    modes[-1] = '1'  # output position should always in immediate mode
+    modes[-1] = "1"  # output position should always in immediate mode
+    if op == 3:
+        modes[0] = "1"
     return op, modes
 
 
 def get_params(modes, start_pos, number=3):
     res = []
     for mode, index in zip(modes, range(start_pos, start_pos + number)):
-        if index < len(data):
-            res.append(data[index] if mode == '1' else data[data[index]])
-        else:
+        try:
+            res.append(data[index] if mode == "1" else data[data[index]])
+        except IndexError:
             res.append(0)  # dummy data
     return res
 
 
-op_shift_num = [0, 4, 4, 2, 2, 3, 3, 4, 4, 1]
+op_shift_num = [0, 4, 4, 2, 2, 3, 3, 4, 4]
 i = 2
 while i < len(data):
     code = data[i]
@@ -35,7 +37,7 @@ while i < len(data):
     elif op == 2:
         data[outpos] = first * second
     elif op == 3:
-        raise RuntimeError('no input given')
+        raise RuntimeError("no input given")
     elif op == 4:
         print(first)
     elif op == 5:
